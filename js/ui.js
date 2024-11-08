e3b5d068113d9f3a05ebce5adf10813fa8a95c15
TontonBanton <pancla13ulas@gmail.com>
2024-11-08 16:36:48 +0800

    Manifest json file added

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,13 @@
+/*DOMContentLoaded
+  This ensures that the code inside this function runs only after the entire HTML document has been loaded.
+  It’s essential for cases where the JavaScript tries to access elements in the DOM before they exist.
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
+
+  //M.Sidenav.init() is a Materialize method that initializes the Sidenav component on the selected elements.
+  //This line initializes a side navigation menu (with elements stored in menus/form) to slide in from the right/left side of the screen.
+
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
   M.Sidenav.init(menus, {edge: 'right'});
