27577799128a01b5da1fa8aa20a356e3d7a0e761
TontonBanton <pancla13ulas@gmail.com>
2024-11-26 12:07:36 +0800

    Storing URL assets on Cache at install sw

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//REGISTERING THE SERVICE WORKER (sw.js)
+
 if ('serviceWorker' in navigator) {            //Navigator - object for the browser used
   navigator.serviceWorker.register('/sw.js')   //Register the sw.js as a service worker
   .then((regObj) => console.log('Service worker is registered', regObj))
--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-//install service worker
-self.addEventListener('install', (evt) => {
-  console.log('Service worker has been installed')
-})
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
 
-//activate service worker
+// install event
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+//SERVICE WORKER ACTIVATE check
 self.addEventListener('activate', (evt) => {
   console.log('Service worker has been activated')
 })
 
-//fetch event
+//fetch event (server request) listener
 self.addEventListener('fetch', (evt) => {
   console.log('fetched events', evt)
 })
